17260d5e6bfbe309931db8876d8322cc15a03831
Edward Tanguay (windows) <edward@tanguay.info>
2023-01-31 02:23:56 +0100

    finished

--- src/pages/PageDashboard.tsx
+++ src/pages/PageDashboard.tsx
@@ -1,7 +1,20 @@
+import { useContext } from 'react';
+import { AppContext } from '../AppContext';
+
 export const PageDashboard = () => {
+	const { fileItems } = useContext(AppContext);
+
 	return (
 		<div className="page pageDashboard">
-			<p>This is the Dashboard page.</p>
+			<h2>There are {fileItems.length} files uploaded.</h2>
+			<ul>
+				{fileItems.map((fileItem, index) => {
+					return (
+						<li key={index}>{fileItem.title}</li>
+					)
+				})}
+			</ul>
+
 		</div>
 	);
 };
